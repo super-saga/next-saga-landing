61cca8caa39ab6dce459f21896eea57fc64cd309
Dede Kurniawan <dedekrnwan.official@gmail.com>
2025-12-21 03:44:24 +0700

    manual trigger

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -438,7 +438,6 @@ const MobileApp = () => (
                </div>
             </div>
           </motion.div>
-          
           {/* Decorative blobs */}
           <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-[500px] h-[500px] bg-primary/20 rounded-full blur-3xl -z-10" />
         </div>
